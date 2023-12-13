c788c686a88142c390a22df06afe37d8d62856de
debasish1452003 <debasishrana1452003@gmail.com>
2023-12-13 22:23:57 +0530

    added notification section

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,28 @@ import React from "react";
 import Layout from "../components/Layouts/Layout";
 import Design from "../components/Layouts/Design";
 
+// import { Swiper, SwiperSlide } from "swiper/react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+// import React, { useRef, useState } from "react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import "swiper/css/effect-coverflow";
+
+// Import Swiper styles
+import "swiper/css";
+
+import {
+  Autoplay,
+  EffectCoverflow,
+  Navigation,
+  Pagination,
+} from "swiper/modules";
+
 const HomePage = () => {
   return (
     <Layout>
@@ -11,6 +33,83 @@ const HomePage = () => {
           <h1 className="gradient-text">ASME</h1>
           <p className="change-text"></p>
         </div>
+      </div>
+      <div className="notify">
+        <h1>LATEST UPDATES</h1>
+        <Swiper
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={"3"}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          loop={true}
+          navigation={true}
+          modules={[EffectCoverflow, Pagination, Autoplay, Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <img
+              src="https://i.postimg.cc/G2828yhd/Whats-App-Image-2023-12-13-at-10-10-06-PM.jpg"
+              alt="carousel"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://i.postimg.cc/j5pjzSLN/Whats-App-Image-2023-12-13-at-10-11-06-PM.jpg"
+              alt="carousel"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://i.postimg.cc/T3fp71QJ/Whats-App-Image-2023-12-13-at-10-11-06-PM-1.jpg"
+              alt="carousel"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://i.postimg.cc/vmSTZzy0/Whats-App-Image-2023-12-13-at-10-12-12-PM.jpg"
+              alt="carousel"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://i.postimg.cc/htshHn5W/Whats-App-Image-2023-12-13-at-10-13-33-PM.jpg"
+              alt="carousel"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://i.postimg.cc/3JMyGnv6/Whats-App-Image-2023-12-13-at-10-14-08-PM.jpg"
+              alt="carousel"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://i.postimg.cc/hvmfw7Hp/Whats-App-Image-2023-12-13-at-10-15-41-PM.jpg"
+              alt="carousel"
+            />
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <img
+              src=" https://i.postimg.cc/nr5tWr9M/Whats-App-Image-2023-12-13-at-11-15-23-AM.jpg"
+              alt="Carousel"
+            />
+          </SwiperSlide>
+        </Swiper>
       </div>
     </Layout>
   );
