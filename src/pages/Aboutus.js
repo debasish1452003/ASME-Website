f52e23b785ff6e6eba7fbb5b32c6912761d4dfa9
Sid <dassidharthasekhar@gmail.com>
2024-01-20 19:35:10 +0530

    added Projects and Events sections

--- src/pages/Aboutus.js
+++ src/pages/Aboutus.js
@@ -6,9 +6,8 @@ const Aboutus = () => {
   return (
     <Layout title={"ASME-NITRKL Aboutus"}>
       <div className="bgHeading">
-<<<<<<< HEAD
         <img className="bgImage" src="https://images.pexels.com/photos/586570/pexels-photo-586570.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-        <h1 className="heading">ABOUT US</h1>
+        <h1>ABOUT US</h1>
       </div>
 
       <div className='bg'>
@@ -45,45 +44,6 @@ const Aboutus = () => {
         </div>
       </div>
 
-=======
-        <img
-          className="bgImage"
-          src="https://images.pexels.com/photos/586570/pexels-photo-586570.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt="aboutUs"
-        />
-        <h1 className="heading">About Us</h1>
-      </div>
-
-      <div className="bg">
-        <div className="aboutUsContent">
-          <p>
-            ASME NITR Chapter stands out as one of the most active technical
-            society in the eastern zone of India. The ASME NIT Rourkela Chapter
-            is a student chapter of the American Society of Mechanical Engineers
-            (ASME) International, located in the National Institute of
-            Technology, Rourkela, India. The chapter was established in 2012 by
-            the students of the 2010 batch. The chapter aims to promote the
-            science and practice of mechanical engineering among students by
-            organizing seminars, training workshops, and awarding prizes. The
-            chapter has been actively participating in various technical events
-            and competitions. The chapter is one of the most prominent, active,
-            and dynamic student sections of ASME International. As a student
-            organization on campus, our goal is to provide a place for both
-            networking and information, offering guest speakers, workshops and
-            more. Its vision is to become a premier institution for promoting
-            art, science and the practice of mechanical and multi- disciplinary
-            engineering and the allied sciences to our diverse communities and
-            opening up new horizons for the students to stand out globally.
-          </p>
-        </div>
-        <div className="right_content">
-          <h4 className="h4_left">Passion in the new Human powered vehicle</h4>
-          <h4 className="h4_right">High-Quality design craft</h4>
-        </div>
-      </div>
-
-      <div className="lowerContent"></div>
->>>>>>> 2eb55b739a016347a98de9ca248695e43b98f330
     </Layout>
   );
 };
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,31 +1,53 @@
 import React from "react";
 import Layout from "../components/Layouts/Layout";
 import { Link } from "react-router-dom";
+import "../style/projects.css";
 // import BgVideo from "../components/dummy/BgVideo";
 
 const Projects = () => {
   return (
-    <Layout>
-      <>
-        <div
-          className="main-events"
-          style={{ height: "70vh", marginTop: "75px" }}
-        >
-          {
-            <img
-              src="https://i.postimg.cc/Njz8FWQV/octavian-dan-b21-Ty33-Cq-Vs-unsplash.jpg"
-              alt="Projects"
-              style={{
-                width: "100%",
-                height: "100%",
-                objectFit: "cover",
-              }}
-            />
-          }
+    <Layout title={"ASME-NITRKL Projects"}>
+      <div className="heading">
+        <img className="bgImage" src="https://images.pexels.com/photos/3757226/pexels-photo-3757226.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+        <h1>PROJECTS</h1>
+      </div>
+
+      <div className="main-content">
+      <div className="project"> 
+        <img src="https://i.postimg.cc/d3v666rJ/mate-vanyek-OYn-U6-Vs-P3-M-unsplash.jpg" alt="" /></div>
+        <div className="project">
+          <h3>BLUESTREAK</h3>
+          <p>Bluestreak, a team within ASME at NIT Rourkela, is a dedicated team focused on research, analysis, and innovation. They specialize in designing human-powered vehicles from scratch, incorporating indigenous elements. Bluestreak actively participates in the eHPV competition at ASME Efest, showcasing their commitment to pushing the boundaries of human-powered vehicle design and contributing to the advancement of sustainable transportation solutions.
+          </p></div>
+
+          <div className="project"> 
+        <img src="https://i.postimg.cc/d3v666rJ/mate-vanyek-OYn-U6-Vs-P3-M-unsplash.jpg" alt="" /></div>
+        <div className="project">
+          <h3>BLUEPRINT</h3>
+          <p>
+            Team Blueprint, a team of ASME NIT Rourkela, engages in research, innovation, and design. They utilize CAD software to create diverse models, subsequently manufacturing them through 3D printing technology. The team actively participates in the IAM3D competition as part of ASME Efest, showcasing their skills and expertise in engineering and design within the ASME community.
+          </p>
         </div>
-      </>
+        <div className="project"> 
+        <img src="https://i.postimg.cc/d3v666rJ/mate-vanyek-OYn-U6-Vs-P3-M-unsplash.jpg" alt="" /></div>
+        <div className="project">
+          <h3>BLUEBIRD</h3>
+          <p>Bluebird, a team from ASME NIT Rourkela, specializes in CAD modeling and coding for virtual competitions. Their expertise extends to designing vehicles for diverse challenges, including autonomous vehicle competitions, lunar lander simulations, and ocean cleanup missions. Through innovation and technical prowess, Team Bluebird consistently participates in and excels at a variety of virtual events, showcasing their commitment to engineering excellence and problem-solving in the digital realm.",
+          </p>
+        </div>
+
+        <div className="project"> 
+        <img src="https://i.postimg.cc/d3v666rJ/mate-vanyek-OYn-U6-Vs-P3-M-unsplash.jpg" alt="" /></div>
+        <div className="project">
+          <h3>OLD GUARD</h3>
+          <p>Engineers, like other professionals, need strong communication skills in both speaking and writing. The Oral Competition, supported by the ASME Old Guard Committee, underscores the importance of effective oral presentations. The committee's annual contributions and volunteer efforts drive this program, benefiting engineering students and early career professionals, ultimately enhancing the engineering profession.
+
+          </p>
+        </div>
+      </div>
     </Layout>
   );
 };
 
 export default Projects;
+
