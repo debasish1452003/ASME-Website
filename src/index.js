92b43ad973d025683c371fa78a5ee2213915d68b
debasish1452003 <debasishrana1452003@gmail.com>
2024-07-04 14:33:39 +0530

    error commit browser router

--- src/index.js
+++ src/index.js
@@ -8,15 +8,15 @@ import "./style/achievements.css";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
