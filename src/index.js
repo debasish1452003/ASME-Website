a96cb96c8bad0bf816146a8d980f49177ddcf2e7
debasish1452003 <debasishrana1452003@gmail.com>
2024-07-07 23:21:48 +0530

    404 commit

--- src/index.js
+++ src/index.js
@@ -8,15 +8,15 @@ import "./style/achievements.css";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </HashRouter>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
